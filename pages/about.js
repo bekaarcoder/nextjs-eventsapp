ae304b474cb0c7842ac5963c3eb4e36c6cea7c4d
Shashank <s_shashank@outlook.com>
2021-05-22 20:31:58 +0530

    initial page and routes setup

---
+++ pages/about.js
@@ -0,0 +1,12 @@
+export default function about() {
+    return (
+        <div>
+            <h1>About</h1>
+            <p>
+                Find all the latest rock events and other musical events around
+                you.
+            </p>
+            <p>Version: 1.0.0</p>
+        </div>
+    );
+}
